00d242e8f76c9143f95adb2aa9f2b689bff11c76
alfphiee <alfiebinnie@gmail.com>
2024-02-22 16:51:15 +0000

    Setup Project API endpoints - basic level

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    STATUS_CHOICES = [
+        ('PL', 'Planning'),
+        ('IP', 'In Progress'),
+        ('CO', 'Completed'),
+        ('OH', 'On Hold')
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_projects')
+    collaborators = models.ManyToManyField(User, related_name='projects', blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='PL')
+    github_url = models.URLField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
