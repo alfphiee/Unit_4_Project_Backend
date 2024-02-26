50170005959479468651a664df6c91e4637eaf37
alfphiee <alfiebinnie@gmail.com>
2024-02-26 09:47:40 +0000

    Setup basic task api

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('', views.ProjectViewSet)
+router.register('', views.ProjectViewSet, basename='project')
 
-urlpatterns = [
-    path('', include(router.urls))
-]
+project_router = routers.NestedDefaultRouter(router, '', lookup='project')
+project_router.register('tasks', views.TaskViewSet, basename='project-tasks')
+
+urlpatterns = router.urls + project_router.urls
