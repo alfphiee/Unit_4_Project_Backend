12672a1ec5cc598b977028fecaa03e4d96486c88
alfphiee <alfiebinnie@gmail.com>
2024-02-28 17:24:21 +0000

    Added ability to read user from token

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,13 @@
 from .models import Project, Task
 from rest_framework import permissions, viewsets
 from django.db.models import Q
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import *
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
+    authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
         user = self.request.user.id
