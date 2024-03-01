5872f5f4148844a40e107ae39bb8e03c5499a2a1
alfphiee <alfiebinnie@gmail.com>
2024-03-01 02:39:39 +0000

    Implemented Photo model

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,11 +5,37 @@ from rest_framework import serializers
 
 class ProjectSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
+    collaborators_emails = serializers.ListField(child=serializers.EmailField(), write_only=True, required=False)
     collaborators = UserSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
-        fields = ['id', 'title', 'description', 'start_date', 'end_date', 'status', 'github_url', 'created_at', 'updated_at', 'owner', 'collaborators']
+        fields = ['id', 'title', 'description', 'start_date', 'end_date', 'status', 'github_url', 'created_at', 'updated_at', 'owner', 'collaborators', 'collaborators_emails']
 
+    def create(self, validated_data):
+        collaborators_emails = validated_data.pop('collaborators_emails', [])
+        project = Project.objects.create(**validated_data)
+        self._update_collaborators(project, collaborators_emails)
+        return project
+
+    def update(self, instance, validated_data):
+        collaborators_emails = validated_data.pop('collaborators_emails', [])
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        self._update_collaborators(instance, collaborators_emails)
+        return instance
+
+    def _update_collaborators(self, project, emails):
+        if emails:
+            collaborators = []
+            for email in emails:
+                try:
+                    user = User.objects.get(email=email)
+                    collaborators.append(user)
+                except User.DoesNotExist:
+                    pass
+            project.collaborators.set(collaborators)
 
 class TaskSerializer(serializers.ModelSerializer):
     assignee_id = serializers.IntegerField(allow_null=True, required=False, write_only=True)
@@ -28,7 +54,6 @@ class TaskSerializer(serializers.ModelSerializer):
         return Task.objects.create(project_id=project_id, **validated_data)
 
     def to_representation(self, instance):
-        """Modify the representation of the serializer to include user details."""
         representation = super(TaskSerializer, self).to_representation(instance)
         representation['assignee'] = UserSerializer(instance.assignee).data if instance.assignee else None
         return representation
--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for user_id {self.user.id} @ {self.url}"
